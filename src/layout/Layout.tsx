dd6ed9ac0a097c809b6b3fee74a61e92beb917d7
p-alex <alexey9021@gmail.com>
2023-04-18 13:58:40 +0300

    Setup tailwindcss

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,10 @@
 import { Outlet } from "react-router-dom";
 const Layout = () => {
-  return <>{<Outlet />}</>;
+  return (
+    <>
+      <main>{<Outlet />}</main>
+    </>
+  );
 };
 
 export default Layout;
