579ca759c3b16eccaee19ea97a548512e7b5fb49
p-alex <alexey9021@gmail.com>
2023-04-21 19:21:15 +0300

    make tests active

--- src/components/ReservationForm.test.tsx
+++ src/components/ReservationForm.test.tsx
@@ -1,43 +1,44 @@
-import { act, render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import ReservationForm from "./ReservationForm";
+
 describe("reservation form", () => {
   const fetchAPI = jest.fn();
-  xit("has heading with text 'Book now'", async () => {
+  it("has heading with text 'Book now'", async () => {
     render(<ReservationForm fetchAPI={fetchAPI} />);
     const h1 = await screen.findByRole("heading", { level: 1 });
     expect(h1).toHaveTextContent("Book now");
   });
 
-  xit("should have a date input with label 'Choose date'", () => {
+  it("should have a date input with label 'Choose date'", () => {
     render(<ReservationForm fetchAPI={fetchAPI} />);
-    const dateInput = screen.getByLabelText(/Choose date/);
+    const dateInput = getDateInput();
     const inputType = dateInput.getAttribute("type");
     expect(inputType).toBe("date");
   });
 
-  xit("should have a select input with label 'Choose time'", () => {
+  it("should have a select input with label 'Choose time'", () => {
     render(<ReservationForm fetchAPI={fetchAPI} />);
-    const selectInput = screen.getByLabelText(/Choose time/);
+    const selectInput = getTimeSelect();
     const nodeName = selectInput.nodeName;
     expect(nodeName).toBe("SELECT");
   });
 
-  xit("should have a number input with label 'How many guests?'", () => {
+  it("should have a number input with label 'How many guests?'", () => {
     render(<ReservationForm fetchAPI={fetchAPI} />);
-    const input = screen.getByLabelText(/How many guests?/);
+    const input = getGuestsInput();
     const inputType = input.getAttribute("type");
     expect(inputType).toBe("number");
   });
 
-  xit("should have a select input with label 'Occasion'", () => {
+  it("should have a select input with label 'Occasion'", () => {
     render(<ReservationForm fetchAPI={fetchAPI} />);
     const selectInput = screen.getByLabelText(/Occasion/);
     const inputType = selectInput.nodeName;
     expect(inputType).toBe("SELECT");
   });
 
-  xit("should have a submit button", async () => {
+  it("should have a submit button", async () => {
     render(<ReservationForm fetchAPI={fetchAPI} />);
     const submitButton = await screen.findByRole("button");
     expect(submitButton).toBeInTheDocument();
